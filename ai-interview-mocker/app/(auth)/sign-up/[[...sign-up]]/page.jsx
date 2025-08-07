77598aa52053ad537c63ad713401ded20f49504c
Ankana Saha <130166084+Codebrokerrose@users.noreply.github.com>
2025-08-07 15:37:54 +0530

    Update page.jsx

--- ai-interview-mocker/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ ai-interview-mocker/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,21 +2,22 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-<section className="grid h-screen grid-cols-1 bg-gray-50 sm:grid-cols-2 overflow-hidden">
+    <section className="grid h-screen grid-cols-1 sm:grid-cols-2 bg-gray-50 overflow-hidden">
       {/* Form Section */}
-      <div className="flex h-full items-center justify-center p-6 sm:p-12">
+      <div className="flex h-full items-center justify-center p-6 sm:p-12 bg-white">
         <div className="w-full max-w-md">
-            
           <SignUp />
         </div>
       </div>
 
       {/* Image Section */}
-      <img
-        alt="Login illustration"
-        src="https://image.slidesdocs.com/responsive-images/docs/picture-of-artificial-intelligence-in-science-and-technology-page-border-background-word-template_dfd84202d0__1131_1600.jpg"
-        className="h-full w-full object-cover"
-      />
-    </section>  
-);
+      <div className="relative hidden sm:block">
+        <img
+          alt="Login illustration"
+          src="https://image.slidesdocs.com/responsive-images/docs/picture-of-artificial-intelligence-in-science-and-technology-page-border-background-word-template_dfd84202d0__1131_1600.jpg"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+    </section>
+  );
 }
